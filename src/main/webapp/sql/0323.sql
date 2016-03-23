87f38be08903cf6807a152d2c525f82a6d785023
yc122166131 <yc122166131@vip.qq.com>
2016-03-23 22:27:44 +0800

    sql

---
+++ src/main/webapp/sql/0323.sql
@@ -0,0 +1,105 @@
+/*
+SQLyog Ultimate v12.08 (64 bit)
+MySQL - 5.5.38 : Database - db_shiro
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_shiro` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `db_shiro`;
+
+/*Table structure for table `t_department` */
+
+DROP TABLE IF EXISTS `t_department`;
+
+CREATE TABLE `t_department` (
+  `id` int(8) NOT NULL,
+  `departmentName` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_department` */
+
+insert  into `t_department`(`id`,`departmentName`) values (1003,'开发部'),(1005,'美工部'),(10086,'产品部'),(90001,'运维部');
+
+/*Table structure for table `t_permission` */
+
+DROP TABLE IF EXISTS `t_permission`;
+
+CREATE TABLE `t_permission` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `permissionName` varchar(50) DEFAULT NULL,
+  `roleId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `roleId` (`roleId`),
+  CONSTRAINT `t_permission_ibfk_1` FOREIGN KEY (`roleId`) REFERENCES `t_role` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_permission` */
+
+insert  into `t_permission`(`id`,`permissionName`,`roleId`) values (1,'user:*',1),(2,'user:read',2);
+
+/*Table structure for table `t_role` */
+
+DROP TABLE IF EXISTS `t_role`;
+
+CREATE TABLE `t_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `roleName` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_role` */
+
+insert  into `t_role`(`id`,`roleName`) values (1,'admin'),(2,'manager'),(3,'employee');
+
+/*Table structure for table `t_user` */
+
+DROP TABLE IF EXISTS `t_user`;
+
+CREATE TABLE `t_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userName` varchar(20) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL COMMENT '电子邮箱',
+  `roleId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `roleId` (`roleId`),
+  CONSTRAINT `t_user_ibfk_1` FOREIGN KEY (`roleId`) REFERENCES `t_role` (`id`),
+  CONSTRAINT `t_user_ibfk_2` FOREIGN KEY (`id`) REFERENCES `t_user_detail` (`userid`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_user` */
+
+insert  into `t_user`(`id`,`userName`,`password`,`email`,`roleId`) values (1,'java','123','cc@qq.com',1),(2,'jack','123','aa@sohu.com',2),(3,'marry','123','bb@sina.com',3),(4,'json','123','dd@gmail.com',3),(5,'js','123','js@github.com',3),(6,'spring','123','spring@github.com',3),(7,'lucene','123','lucene@hotmail.com',3);
+
+/*Table structure for table `t_user_detail` */
+
+DROP TABLE IF EXISTS `t_user_detail`;
+
+CREATE TABLE `t_user_detail` (
+  `userid` int(10) NOT NULL AUTO_INCREMENT,
+  `sex` int(1) NOT NULL,
+  `favourate` varchar(50) NOT NULL,
+  `departmentId` int(8) NOT NULL,
+  PRIMARY KEY (`userid`),
+  KEY `departmentId` (`departmentId`),
+  CONSTRAINT `t_user_detail_ibfk_1` FOREIGN KEY (`departmentId`) REFERENCES `t_department` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_user_detail` */
+
+insert  into `t_user_detail`(`userid`,`sex`,`favourate`,`departmentId`) values (1,1,'1,2,3',90001),(2,0,'2,3',10086),(3,1,'1,3',1003),(4,1,'3',1005),(5,1,'2,3',1003),(6,0,'1,2,3',1003),(7,0,'2',1005);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
